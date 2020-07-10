f1fe6e7222b87a2d0ae893f819d63bb266e1e0f4
Ausgard <serhiirodik@gmail.com>
2020-07-10 20:48:15 +0300

    Отправка данных по ajax

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -1,13 +1,11 @@
-const requestURL = 'https://jsonplaceholder.typicode.com/users'
-const xhr = new XMLHttpRequest()
+const requestURL = 'https://jsonplaceholder.typicode.com/posts'
 let popupBtn = document.querySelector('.footer__popup')
 let contactForm = document.querySelector('.contact-form')
 let contactFormBtn = document.querySelector('.contact-form__btn')
-
 let inputName = document.querySelector('.contact-form__name input')
 let inputEmail = document.querySelector('.contact-form__e-mail input')
 let inputMessage = document.querySelector('.contact-form__message textarea')
-
+let successBlock = document.querySelector('.success-popup')
 popupBtn.addEventListener('click', popupHandler)
 window.addEventListener('keydown', popupHandler)
 contactFormBtn.addEventListener('click', formHandler)
@@ -23,6 +21,9 @@ function popupHandler(event) {
 
 function formHandler() {
     event.preventDefault()
+    let formData
+    let formDataJson 
+    let validationSwitch
     let separator = ' ';
     nameArr = inputName.value.split(separator)
     emailArr = inputEmail.value.split(separator)
@@ -34,11 +35,13 @@ function formHandler() {
                 document.querySelector('.contact-form__name input[placeholder]').style.backgroundColor = '#ff020226';
                 inputName.setCustomValidity('Заполните в формате: "Имя Фамилия"')
                 flag = false
+                validationSwitch = false
             }
             else {
                 inputName.setCustomValidity('')
                 document.querySelector('.contact-form__name input[placeholder]').style.backgroundColor = '#28ff0226';
                 flag = true
+                validationSwitch = true
             }
         }
       
@@ -50,6 +53,7 @@ function formHandler() {
             }
             inputName.value = nameArrMod.join(' ')
         }
+        return validationSwitch
     }
     nameValidation(nameArr)
 
@@ -58,18 +62,23 @@ function formHandler() {
             if (emailArr.length >= 2 || emailArr[i].length === 0) {
                 document.querySelector('.contact-form__e-mail input[placeholder]').style.backgroundColor = '#ff020226';
                 inputEmail.setCustomValidity('Заполните в формате: "name@email.com"')
+                validationSwitch = false
             } else {
                 inputName.setCustomValidity('')
                 document.querySelector('.contact-form__e-mail input[placeholder]').style.backgroundColor = '#28ff0226';
+                validationSwitch = true
             }
         }
         if (emailArr[0].includes('@')) {
             inputName.setCustomValidity('')
             document.querySelector('.contact-form__e-mail input[placeholder]').style.backgroundColor = '#28ff0226';
+            validationSwitch = true
         } else {
             document.querySelector('.contact-form__e-mail input[placeholder]').style.backgroundColor = '#ff020226';
             inputEmail.setCustomValidity('Заполните в формате: "name@email.com"')
+            validationSwitch = false
         }
+        return validationSwitch
     }
     emailValidation(emailArr)
 
@@ -77,20 +86,43 @@ function formHandler() {
         if (inputMessage.value.length < 20) {
             document.querySelector('.contact-form__message textarea[id=client-message][placeholder]').style.backgroundColor = '#ff020226';
             inputMessage.setCustomValidity('Длина сообщения должна быть не менее 20 символов')
+            validationSwitch = false
         } else {
             document.querySelector('.contact-form__message textarea[id=client-message][placeholder]').style.backgroundColor = '#28ff0226';
             inputMessage.setCustomValidity('')
+            validationSwitch = true
         }
+        return validationSwitch
+
     }
     messageValidation(inputMessage)
-    console.log(inputMessage.value.length)
+
+    if (validationSwitch) {
+        formData = {
+            name: nameArr,
+            email: emailArr,
+            message: inputMessage.value
+        }
+        formDataJson = JSON.stringify(formData)
+        let xhr = new XMLHttpRequest()
+        xhr.responseType = 'json'
+        xhr.open('POST', requestURL)
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+        xhr.onload = function() {
+            if (xhr.readyState === 4) {
+                createAnswer ()
+            }
+        }
+        xhr.send(formDataJson)
+    }
+    function createAnswer () {
+        contactForm.classList.add('hidden')
+        successBlock.classList.remove('hidden')
+        window.addEventListener('click', closeSuccessBlock)
+        function closeSuccessBlock (event) {
+            if (!successBlock.classList.contains('hidden') && contactForm.classList.contains('hidden') && event.which === 1) {
+                successBlock.classList.add('hidden')
+            }
+        }  
+    }
 }
-
-// xhr.open('GET', requestURL)
-// xhr.responseType = 'json'
-
-// xhr.onload = function() {
-//     console.log(xhr)
-// }
-
-// xhr.send()
